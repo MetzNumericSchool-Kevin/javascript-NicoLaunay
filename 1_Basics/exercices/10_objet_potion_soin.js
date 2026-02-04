00ed2e52ac8d09e3b222625ab13a13f3cdfac527
Nicolas <nicolas.launay.0@gmail.com>
2026-02-04 09:18:56 +0100

    Rangeons les informations de la potion de soin dans un objet

--- 1_Basics/exercices/10_objet_potion_soin.js
+++ 1_Basics/exercices/10_objet_potion_soin.js
@@ -1,1 +1,16 @@
 // Rangeons les informations de la potion de soin dans un objet 🧹
+
+// **Fichier** : `exercices/10_objet_potion_soin.js`
+
+// - Créé un objet pour stocker les informations d'une potion de soin, tu auras dans cet objet :
+//   - le nom de la potion
+//   - son prix
+//   - le stock
+// - Affiche directement l'objet dans la console
+
+let health_potion = {
+    "name": "Potion de soin",
+    "price": 50,
+    "stock": 10
+}
+console.log(health_potion)
