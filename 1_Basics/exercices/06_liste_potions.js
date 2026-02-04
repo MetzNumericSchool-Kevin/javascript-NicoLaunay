f812130dec4d3bf5bceed7cdb22a625d5b2a5a6b
Nicolas <nicolas.launay.0@gmail.com>
2026-02-04 09:01:30 +0100

    Liste des potions

--- 1_Basics/exercices/06_liste_potions.js
+++ 1_Basics/exercices/06_liste_potions.js
@@ -1,1 +1,9 @@
 // Liste des potions
+
+// **Fichier** : `exercices/06_liste_potions.js`
+
+// - Créé un tableau qui contient les noms de trois potions que tu vends dans ta boutique.
+// - Affiche le tableau directement dans la console.
+
+let available_potions = ["Potion de soin", "Potion de mana", "Poison"]
+console.log(available_potions)
