18551d11f7d010d63fa703667438c8c8bdf3f9f8
Nicolas <nicolas.launay.0@gmail.com>
2026-02-04 09:23:17 +0100

    Affichons les informations de la potion

--- 1_Basics/exercices/11_affichage_infos_potion.js
+++ 1_Basics/exercices/11_affichage_infos_potion.js
@@ -1,8 +1,14 @@
 // Affichons les informations de la potion
 
+// **Fichier** : `exercices/11_affichage_infos_potion.js`
+
+// - Affiche le nom de la potion en utilisant la notation pointée `.`
+// - Affiche le prix de la potion en utilisant la notation crochet `[]`
 // Variable de l'exercice précédent (modifie selon tes valeurs)
-const potion_soin = {
-  nom: "Potion de soin",
-  prix: 50,
+const health_potion = {
+  name: "Potion de soin",
+  price: 50,
   stock: 10,
 };
+console.log(health_potion.name)
+console.log(health_potion["name"])
