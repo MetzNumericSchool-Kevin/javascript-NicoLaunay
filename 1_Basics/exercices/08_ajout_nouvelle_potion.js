9f13bdc32531472020ba32755babc9f3aa3757ae
Nicolas <nicolas.launay.0@gmail.com>
2026-02-04 09:12:24 +0100

    Ajout d'une nouvelle potion

--- 1_Basics/exercices/08_ajout_nouvelle_potion.js
+++ 1_Basics/exercices/08_ajout_nouvelle_potion.js
@@ -1,4 +1,15 @@
 // Ajout d'une nouvelle potion
 
+// **Fichier** : `exercices/08_ajout_nouvelle_potion.js`
+
+// - Ajoute une nouvelle potion dans le tableau en utilisant la méthode `push` de l'objet Array.
+// - Affiche la liste des potions mis à jour
+// - Finalement, nous ne gardons pas cette nouvelle potion dans la liste. Supprime la en utilisant la méthode `pop`
+// - Affiche la liste des potions mis à jour
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const potions = ["Potion de soin", "Potion de mana", "Potion d'endurance"];
+potions.push("Metamphetamine")
+console.log(potions)
+potions.pop()
+console.log(potions)
+
