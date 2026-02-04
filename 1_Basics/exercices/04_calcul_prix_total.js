0c4a300af95f16d257032e05cf1a6282deaf33d0
Nicolas <nicolas.launay.0@gmail.com>
2026-02-04 08:48:35 +0100

    Calcul du prix total d'une commande de potion

--- 1_Basics/exercices/04_calcul_prix_total.js
+++ 1_Basics/exercices/04_calcul_prix_total.js
@@ -1,4 +1,8 @@
 // Calcul du prix total d'une commande de potion 🪙
+// **Fichier** : `exercices/04_calcul_prix_total.js`
 
+// Demande avec `prompt` une quantité de potion de soin, et affiche ensuite dans la `console` => `Prix de <quantite_potion> potions de soins : <prix_total> 🪙 mon cher Aventurier. 💸`.
 // Variables des exercices précédents (modifie selon tes valeurs)
-let prix_potion_soin = 50;
+let price_health_potion = 50;
+let nb_order = prompt('Combien de potions de soin ?')
+console.log(`Prix de ${nb_order} potions de soins : ${nb_order*price_health_potion} 🪙 mon cher Aventurier. 💸`)
