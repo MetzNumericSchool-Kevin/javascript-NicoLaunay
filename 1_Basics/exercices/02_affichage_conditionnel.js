efa4dcbc7cb4c472395adf6c5f489a0ee557718a
Nicolas <nicolas.launay.0@gmail.com>
2026-02-04 08:28:22 +0100

    Affichage conditionnel

--- 1_Basics/exercices/02_affichage_conditionnel.js
+++ 1_Basics/exercices/02_affichage_conditionnel.js
@@ -1,5 +1,16 @@
 // Affichage conditionnel
+// ### Exercice 2 : Affichage conditionnel
 
+// **Fichier** : `exercices/02_affichage_conditionnel.js`
+
+// Affiche dans ta `console` => `Bienvenue dans la boutique <nom_boutique> Aventurier ! 🎉` si la boutique est ouverte, sinon affiche `La boutique <nom_boutique> est fermée, revenez plus tard Aventurier ! 😴`
 // Variables de l'exercice précédent (modifie selon tes valeurs)
-const nom_boutique = "La Potion Enchantée";
-const boutique_ouverte = true;
+const SHOP_NAME = "La Cruche Fêlée"
+var nb_health_potion = 5
+var price_health_potion = 20
+var is_shop_open = false
+if (is_shop_open) {
+    console.log(`Bienvenue dans la boutique ${SHOP_NAME} Aventurier ! 🎉`)
+} else {
+    console.log(`La boutique ${SHOP_NAME} est fermée, revenez plus tard Aventurier ! 😴`)
+}
